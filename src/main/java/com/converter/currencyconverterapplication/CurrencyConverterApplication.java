d56ed0728361beef057ff3a9dae53d5202af278f
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-01-09 22:12:12 +0200

    SCRUM-3: Add Spring Boot application with Java 21 and Gradle setup

---
+++ src/main/java/com/converter/currencyconverterapplication/CurrencyConverterApplication.java
@@ -0,0 +1,18 @@
+package com.converter.currencyconverterapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+    /**
+     * The main method of the application that starts the Spring Boot application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConverterApplication.class, args);
+    }
+
+}
