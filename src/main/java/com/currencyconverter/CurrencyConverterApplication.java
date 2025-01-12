88c82fa5a97299c89e37a94802778504c0570bf9
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-01-12 18:20:58 +0200

    SCRUM-4: Add dependency management for Spring Boot dependencies BOM

--- src/main/java/com/currencyconverter/CurrencyConverterApplication.java
+++ src/main/java/com/currencyconverter/CurrencyConverterApplication.java
@@ -1,4 +1,4 @@
-package com.converter;
+package com.currencyconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/currencyconverter/CurrencyConverterApplicationTests.java
+++ src/test/java/com/currencyconverter/CurrencyConverterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.converter;
+package com.currencyconverter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
