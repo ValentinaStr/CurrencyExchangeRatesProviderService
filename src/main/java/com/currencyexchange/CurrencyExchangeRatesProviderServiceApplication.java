a643798553dbdf91a1be54ff56436bc1f9518b0d
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-01-18 22:48:37 +0200

    SCRUM-5 : Fix according to Checkstyle rules

--- src/main/java/com/currencyexchange/CurrencyExchangeRatesProviderServiceApplication.java
+++ src/main/java/com/currencyexchange/CurrencyExchangeRatesProviderServiceApplication.java
@@ -3,10 +3,6 @@ package com.currencyexchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * The main class of the Currency Converter application.
- * This class contains the entry point for the application.
- */
 @SpringBootApplication
 public class CurrencyExchangeRatesProviderServiceApplication {
   /**
