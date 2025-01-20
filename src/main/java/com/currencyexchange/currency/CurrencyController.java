26a51e1cf851b9f40bc6357d8354e485bb952769
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-01-20 14:06:22 +0200

    SCRUM-14 : Add Swagger annotations

--- src/main/java/com/currencyexchange/currency/CurrencyController.java
+++ src/main/java/com/currencyexchange/currency/CurrencyController.java
@@ -1,6 +1,13 @@
 package com.currencyexchange.currency;
 
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/currencies")
+@Tag(name = "Currency API", description = "This controller handles operations related to the list of currency names stored in the database.")
 public class CurrencyController {
 
   private final CurrencyService currencyService;
@@ -19,11 +27,19 @@ public class CurrencyController {
   /**
    * Handles GET requests to retrieve a list of all available currencies.
    * This method logs the request and response, then returns the list of all currencies
-   * from the currency service. The list is returned with a status code of 200 (OK).
+   * from the database. The list is returned with a status code of 200 (OK).
    *
    * @return a {@link ResponseEntity} containing a list of currency names
-   *     with an HTTP status code of 200 (OK)
+   * with an HTTP status code of 200 (OK)
    */
+  @Operation(
+      summary = "Get all available currencies",
+      description = "Retrieves a list of all available currencies from the database."
+  )
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successfully retrieved list of currencies",
+          content = @Content(mediaType = "application/json", schema = @Schema(type = "array", description = "List of currency names")))
+  })
   @GetMapping
   public ResponseEntity<List<String>> getAllCurrencies() {
     log.info("Received request to get all currencies.");
