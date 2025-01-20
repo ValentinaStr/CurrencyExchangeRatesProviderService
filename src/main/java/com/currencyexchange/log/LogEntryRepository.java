cefc9ff6e84d3ea5871254c5ee67aabee58bed80
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-01-20 11:22:43 +0200

    SCRUM-5 : Fix according to  requirements

--- src/main/java/com/currencyexchange/log/LogEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.currencyexchange.log;
-
-import com.currencyexchange.log.model.LogEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogEntryRepository extends JpaRepository<LogEntry, Long> {
-}
-
