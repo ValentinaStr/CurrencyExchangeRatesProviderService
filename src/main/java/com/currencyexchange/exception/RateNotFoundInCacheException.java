f7480b8a428b363d09b310c34e36f971cd01f52c
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-01-28 00:29:52 +0200

    SCRUM-7 : Add RateNotFoundInCacheException

---
+++ src/main/java/com/currencyexchange/exception/RateNotFoundInCacheException.java
@@ -0,0 +1,12 @@
+package com.currencyexchange.exception;
+
+public class RateNotFoundInCacheException extends RuntimeException {
+  /**
+   * Constructs a new {@code ExchangeRateNotFoundInCacheException} with the specified detail message.
+   *
+   * @param message The detail message explaining the reason for the exception.
+   */
+  public RateNotFoundInCacheException(String message) {
+    super(message);
+  }
+}
