67561ff63075d8626ec8b3d8fcf10eb43f07fe79
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-02-07 17:31:58 +0200

    SCRUM-11 : Add ExchangeRateClientUnavailableException for handling API unavailability

--- src/main/java/com/currencyexchange/client/FixerClient.java
+++ src/main/java/com/currencyexchange/client/FixerClient.java
@@ -2,20 +2,20 @@ package com.currencyexchange.client;
 
 import com.currencyexchange.business.ApiLogService;
 import com.currencyexchange.dto.ExchangeRateResponseDto;
+import com.currencyexchange.exception.ExchangeRateClientUnavailableException;
 import java.util.Set;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
 @Getter
 @RequiredArgsConstructor
-
 public class FixerClient implements ExchangeRateClient {
 
   @Value("${fixer.api.key}")
@@ -34,9 +34,16 @@ public class FixerClient implements ExchangeRateClient {
     for (String baseCurrency : currency) {
       String url = String.format("%s/latest?access_key=%s&base=%s", apiUrl, apiKey, baseCurrency);
       log.info("Request URL: {}", url);
-      response = restTemplate.getForObject(url, ExchangeRateResponseDto.class);
 
-      apiLogService.logRequest(apiUrl, response);
+      try {
+        response = restTemplate.getForObject(url, ExchangeRateResponseDto.class);
+        apiLogService.logRequest(apiUrl, response);
+
+      } catch (RestClientException e) {
+        log.error("Failed to fetch exchange rates from {}: {}", url, e.getMessage());
+        throw new ExchangeRateClientUnavailableException(
+            "Failed to fetch exchange rates from: " + url);
+      }
     }
     return response;
   }
