4e60f28d97111d11f45ef195e0c9b1ace7cb51b6
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-02-09 22:31:18 +0200

    SCRUM-12 : Update README file: Added information about external currency exchange providers and data update process

--- src/main/java/com/currencyexchange/dto/ExchangeRateDto.java
+++ src/main/java/com/currencyexchange/dto/ExchangeRateDto.java
@@ -4,11 +4,11 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 /**
- * DTO (Data Transfer Object) для представления информации о курсах валют. Содержит валюту и
- * соответствующие ей курсы для других валют.
+ * DTO (Data Transfer Object) for representing currency exchange rate information. It contains a currency
+ * and the corresponding exchange rates for other currencies.
  *
- * @param currency Валюта, для которой получены курсы.
- * @param rates Карта, где ключи — это коды валют, а значения — их курсы относительно валюты {@code
+ * @param currency The currency for which exchange rates are provided.
+ * @param rates A map where keys are currency codes and values are the exchange rates relative to the {@code
  *     currency}.
  */
 public record ExchangeRateDto(String currency, Map<String, BigDecimal> rates) {}
