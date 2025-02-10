8681e094d597dd0b30f857beaba65a1f5be0e426
V.Strazhnikava <v.strazhnikava@godeltech.com>
2025-02-10 11:22:36 +0200

    SCRUM-12 : Fix code style issues according to Checkstyle rules

--- src/main/java/com/currencyexchange/dto/ExchangeRateDto.java
+++ src/main/java/com/currencyexchange/dto/ExchangeRateDto.java
@@ -4,11 +4,11 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 /**
- * DTO (Data Transfer Object) for representing currency exchange rate information. It contains a currency
- * and the corresponding exchange rates for other currencies.
+ * DTO (Data Transfer Object) for representing currency exchange rate information. It contains a
+ * currency and the corresponding exchange rates for other currencies.
  *
  * @param currency The currency for which exchange rates are provided.
- * @param rates A map where keys are currency codes and values are the exchange rates relative to the {@code
- *     currency}.
+ * @param rates A map where keys are currency codes and values are the exchange rates relative to
+ *     the {@code currency}.
  */
 public record ExchangeRateDto(String currency, Map<String, BigDecimal> rates) {}
